7a07b37e67d953aa6e8a0b60e4f78354f08d4871
wenyikun <wenyikun86@gmail.com>
2020-07-28 14:49:47 +0800

    添加

---
+++ serve.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const axios = require('axios')
+
+const instance = axios.create({
+  baseURL: 'http://211.233.81.229:8099/'
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.static(path.resolve(__dirname, './dist')))
+
+app.get('/api/*', (req, res) => {
+  instance.get(req.path, {
+    params: req.query,
+    headers: {
+      usertoken: req.headers.usertoken
+    }
+  }).then(response => {
+    res.json(response.data)
+  })
+})
+
+app.post('/api/*', (req, res) => {
+  instance.post(req.path, {}, {
+    params: req.query,
+    headers: {
+      usertoken: req.headers.usertoken
+    }
+  }).then(response => {
+    res.json(response.data)
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'))
+})
+
+app.listen(8089, () => {
+  console.log('\x1B[32m%s\x1B[39m', 'app listening on port 8089!')
+})
