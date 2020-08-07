7e030ec3ce8405b36c24e172b2168dca54eaefe4
caixiaokun <caixiaokun@caixiaokundeMacBook-Pro-2.local>
2020-08-07 15:36:06 +0800

    s

--- publish.js
+++ publish.js
@@ -30,5 +30,5 @@ function upload(path, remoteDir) {
     .upload()
 }
 
-upload('./serve.js', '/usr/local/tomcat/tomcat8/webapps2')
-// upload('./dist', '/usr/local/tomcat/tomcat8/webapps2')
+// upload('./serve.js', '/usr/local/tomcat/tomcat8/webapps2')
+upload('./dist', '/usr/local/tomcat/tomcat8/webapps2')
